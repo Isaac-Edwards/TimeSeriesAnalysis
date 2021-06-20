3291bf9409294e804147979401e185fc8816930a
Isaac Edwards <isaac.edwards@synaptics.com>
2021-06-20 13:16:07 +0100

    plot decompososition

--- Practice/AustralianDrugs.py
+++ Practice/AustralianDrugs.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
 print(df.head())
@@ -13,3 +14,12 @@ def plot_df(x, y, title='', xlabel='Date', ylabel='Value', dpi=100):
 
 
 plot_df(x=df.index, y=df.value, title='Monthly anti-diabetic drug sales in Australia from 1992 to 2008.')
+
+result_mul = seasonal_decompose(df.value, model='multiplicative', extrapolate_trend='freq')
+result_add = seasonal_decompose(df['value'], model='additive', extrapolate_trend='freq')
+
+plt.rcParams.update({'figure.figsize': (10,10)})
+result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
+result_add.plot().suptitle('Additive Decompose', fontsize=22)
+plt.show()
+
