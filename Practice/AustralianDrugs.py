6033804a36338f7ad98d291bde62528544826a29
Isaac Edwards <isaac.edwards@synaptics.com>
2021-06-20 12:45:08 +0100

    plot the data

--- Practice/AustralianDrugs.py
+++ Practice/AustralianDrugs.py
@@ -1,4 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
 print(df.head())
+
+
+def plot_df(x, y, title='', xlabel='Date', ylabel='Value', dpi=100):
+    plt.figure(figsize=(16,5), dpi=dpi)
+    plt.plot(x,y,)
+    plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
+    plt.show()
+
+
+plot_df(x=df.index, y=df.value, title='Monthly anti-diabetic drug sales in Australia from 1992 to 2008.')
