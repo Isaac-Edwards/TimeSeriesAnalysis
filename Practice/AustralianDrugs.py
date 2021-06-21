2157a645d81cdfbb2a5e62bebf172fa688e7d6af
Isaac Edwards <isaac.edwards@synaptics.com>
2021-06-21 21:40:59 +0100

    Add graphs for detrending and deseasonalising.

--- Practice/AustralianDrugs.py
+++ Practice/AustralianDrugs.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import signal
 from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import adfuller, kpss
 
 df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv', parse_dates=['date'], index_col='date')
 print(df.head())
@@ -10,7 +12,6 @@ def plot_df(x, y, title='', xlabel='Date', ylabel='Value', dpi=100):
     plt.figure(figsize=(16,5), dpi=dpi)
     plt.plot(x,y,)
     plt.gca().set(title=title, xlabel=xlabel, ylabel=ylabel)
-    plt.show()
 
 
 plot_df(x=df.index, y=df.value, title='Monthly anti-diabetic drug sales in Australia from 1992 to 2008.')
@@ -21,5 +22,43 @@ result_add = seasonal_decompose(df['value'], model='additive', extrapolate_trend
 plt.rcParams.update({'figure.figsize': (10,10)})
 result_mul.plot().suptitle('Multiplicative Decompose', fontsize=22)
 result_add.plot().suptitle('Additive Decompose', fontsize=22)
-plt.show()
 
+
+values = df.value.values
+
+# ADF Test
+result = adfuller(values, autolag='AIC')
+print(f'ADF Statistic: {result[0]}')
+print(f'p-value: {result[1]}')
+for key, value in result[4].items():
+    print('Critial Values:')
+    print(f'   {key}, {value}')
+
+# KPSS Test
+result = kpss(values, regression='c')
+print('\nKPSS Statistic: %f' % result[0])
+print('p-value: %f' % result[1])
+for key, value in result[3].items():
+    print('Critial Values:')
+    print(f'   {key}, {value}')
+
+detrended = signal.detrend(df.value.values)
+plt.figure()
+plt.plot(detrended)
+plt.title('Drug Sales detrended by subtracting the least squares fit', fontsize=16)
+
+result_mul = seasonal_decompose(df['value'], model='multiplicative', extrapolate_trend='freq')
+detrended = df.value.values - result_mul.trend
+plt.figure()
+plt.plot(detrended)
+plt.title('Drug Sales detrended by subtracting the trend component', fontsize=16)
+
+# Deseasonalize
+deseasonalized = df.value.values / result_mul.seasonal
+
+# Plot
+plt.figure()
+plt.plot(deseasonalized)
+plt.title('Drug Sales Deseasonalized', fontsize=16)
+plt.plot()
+plt.show()
