0f0225328746882b155dfd3e7646cab136c7cd3d
Isaac Edwards <isaac.edwards@synaptics.com>
2021-06-25 13:55:50 +0100

    Plot bad data

---
+++ Practice/worsedata.py
@@ -0,0 +1,39 @@
+import pmdarima as pm
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/wwwusage.csv', names=['value'], header=0)
+
+model = pm.auto_arima(df.value, start_p=1, start_q=1,
+                      test='adf',       # use adftest to find optimal 'd'
+                      max_p=3, max_q=3, # maximum p and q
+                      m=1,              # frequency of series
+                      d=None,           # let model determine 'd'
+                      seasonal=False,   # No Seasonality
+                      start_P=0,
+                      D=0,
+                      trace=True,
+                      error_action='ignore',
+                      suppress_warnings=True,
+                      stepwise=True)
+
+n_periods = 24
+fc, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+index_of_fc = np.arange(len(df.value), len(df.value)+n_periods)
+
+# make series for plotting purpose
+fc_series = pd.Series(fc, index=index_of_fc)
+lower_series = pd.Series(confint[:, 0], index=index_of_fc)
+upper_series = pd.Series(confint[:, 1], index=index_of_fc)
+
+# Plot
+plt.plot(df.value)
+plt.plot(fc_series, color='darkgreen')
+plt.fill_between(lower_series.index,
+                 lower_series,
+                 upper_series,
+                 color='k', alpha=.15)
+
+plt.title("Final Forecast of WWW Usage")
+plt.show()
