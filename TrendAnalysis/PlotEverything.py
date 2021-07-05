0a456fc68dc4a847e1567792d5e7b329f41a95c7
Isaac Edwards <isaac.edwards@synaptics.com>
2021-07-05 16:02:29 +0100

    Plot everything

--- TrendAnalysis/PlotEverything.py
+++ TrendAnalysis/PlotEverything.py
@@ -7,14 +7,17 @@ df = pd.read_csv(DATA_PATH, index_col='date', parse_dates=True)
 
 issue_codes = df.issue_code.unique()
 
-#for issue in issues:
-single_issue_df = df[df['issue_code'] == issue_codes[0]].copy()
-single_issue_df = single_issue_df.value_counts(['date', 'issue_code'], sort=False).reset_index(name='mentions')
-single_issue_df.set_index('date', inplace=True)
+ax = None
+for issue in issue_codes:
+    single_issue_df = df[df['issue_code'] == issue].copy()
+    single_issue_df = single_issue_df.value_counts(['date', 'issue_code'], sort=False).reset_index(name='mentions')
+    single_issue_df.set_index('date', inplace=True)
 
-single_issue_df['cumulative_mentions'] = single_issue_df['mentions'].cumsum()
+    single_issue_df['cumulative_mentions'] = single_issue_df['mentions'].cumsum()
 
+    if ax is None:
+        ax = single_issue_df['cumulative_mentions'].plot()
+    else:
+        single_issue_df['cumulative_mentions'].plot(ax=ax)
 
-# Plotting
-single_issue_df['cumulative_mentions'].plot()
 plt.show()
